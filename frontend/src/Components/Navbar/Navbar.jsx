9f3bef2ae50adf9e50ef7a328aa34c4a0a9e9a3a
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-14 22:47:54 +0600

    Login page added

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -50,12 +50,13 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="hidden lg:block">
-          <button
+          <Link
+            to="/login"
             type="button"
             className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
           >
             Login
-          </button>
+          </Link>
         </div>
         <div className="lg:hidden">
           <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
