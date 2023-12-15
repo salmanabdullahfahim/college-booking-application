ae483b4006631707ef6c45629f0dd1bf97d756ef
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-15 21:26:25 +0600

    college card design complete

--- frontend/src/Components/Cardsection/CollegeCard.jsx
+++ frontend/src/Components/Cardsection/CollegeCard.jsx
@@ -2,37 +2,44 @@ import React from "react";
 import { ArrowUpRight } from "lucide-react";
 
 const CollegeCard = ({ college }) => {
+  const {
+    name,
+    image,
+    admission_dates,
+    events,
+    research_history,
+    sports_field,
+  } = college;
   return (
     <div className="w-[300px] rounded-md border">
       <img
-        src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        src={image}
         alt="Laptop"
         className="h-[200px] w-full rounded-t-md object-cover"
       />
       <div className="p-4">
         <h1 className="inline-flex items-center text-lg font-semibold">
-          About Macbook &nbsp; <ArrowUpRight className="h-4 w-4" />
+          {name} &nbsp; <ArrowUpRight className="h-4 w-4" />
         </h1>
         <p className="mt-3 text-sm text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-          debitis?
+          Admission Date: {admission_dates}
         </p>
         <div className="mt-4">
           <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-            #Macbook
+            Events: {events.length}
           </span>
-          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-            #Apple
+          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-2 text-[10px] font-semibold text-gray-900">
+            Sports: {sports_field}
           </span>
-          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-            #Laptop
+          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-2 text-[10px] font-semibold text-gray-900">
+            Research: {research_history}
           </span>
         </div>
         <button
           type="button"
           className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
         >
-          Read
+          Details
         </button>
       </div>
     </div>
