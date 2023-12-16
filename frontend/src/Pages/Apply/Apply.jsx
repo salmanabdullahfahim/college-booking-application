05e7983196a8636561f4890e18ff6e445225d2df
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-16 21:47:25 +0600

    Done

--- frontend/src/Pages/Apply/Apply.jsx
+++ frontend/src/Pages/Apply/Apply.jsx
@@ -95,7 +95,7 @@ const Apply = () => {
             className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="subject"
           >
-            College
+            College Name
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
